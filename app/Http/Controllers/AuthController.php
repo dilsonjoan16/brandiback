b3ed3ff2a42a48ec0ba18bc34f9a05c63e50c469
brandonvera <vera.950b@gmail.com>
2022-01-12 08:14:48 -0430

    se agrego facades\Hash

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
